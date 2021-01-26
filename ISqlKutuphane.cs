35c9832bcc08ea6f1b539da818bf19b3e24daf7c
M. Başar Demirci <62293688+basardemm@users.noreply.github.com>
2021-01-26 09:21:51 +0300

    Interface for SqlKutuphane

---
+++ ISqlKutuphane.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeknikServis.Business_Model
+{
+    interface ISqlKutuphane
+    {
+        string DosyaOkuyucu();
+
+        SqlConnection Connector();
+
+        void SqlExecuteNonQuery(string sorgu, SqlParameter[] sdr);
+
+        int SqlExecuteScalar(string sorgu);
+
+        SqlDataReader SqlExecuteReader(string sorgu, SqlParameter[] sdr);
+
+        SqlDataReader SqlExecuteReader2(string sorgu);
+        
+        DataTable SqlBindind(string sorgu);
+
+        DataSet SqlBinding_Rapor(string sorgu);
+    }
+}
